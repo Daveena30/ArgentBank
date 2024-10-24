c352d92b9515f8951b8aa4e8809a35fb90b12518
Daveena <kellyangel2330@gmail.com>
2024-10-24 11:00:18 +0200

    create the Nav component, add export default to the home page,

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,22 +1,28 @@
 import './nav.css'
+import nav_logo from '../../img/argentBankLogo.webp'
+
+const Nav = () => {
+
+    return (
+        <nav>
+            <a href="/">
+                <img
+                src={nav_logo}
+                alt="Argent Bank Logo"
+                />
+                <h1>Argent Bank</h1>
+                </a>
+    
+                <a href="/login">
+                    <i class="fa fa-user-circle"></i>
+                    Sign In
+                </a>
+        </nav>
+    )
+
+}
 
 
 
-return (
-    <nav className="main-nav">
-      <link className="main-nav-logo" href="./index.html">
-        <img
-          className="main-nav-logo-image"
-          src="./img/argentBankLogo.png"
-          alt="Argent Bank Logo"
-        />
-        <h1 className="sr-only">Argent Bank</h1>
-      </link>
-      <div>
-        <link className="main-nav-item" href="./sign-in.html">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </link>
-      </div>
-    </nav>
-)
+
+export default Nav;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,27 @@
+import React from "react";
 import "./login.css"
 
-import React from "react";
+const Login = () => {
+
+    return (
+        <main class="login bg-dark">
+          <section class="sign-in-content">
+            <i class="fa fa-user-circle sign-in-icon"></i>
+            <h2>Sign In</h2>
+            </section>
+            </main>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default Login
