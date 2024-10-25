857a0883d9c63a2596f109d7f4faf4851db8cf8b
Daveena <kellyangel2330@gmail.com>
2024-10-25 13:56:39 +0200

    add const for Email, Password, Remember me,  and add localhost of Backend to the frontend package.json

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "./login.css";
 
@@ -6,13 +6,16 @@ const Login = () => {
 
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); 
-        navigate('/profile'); 
-      };
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
 
-    const [password, setPassword] = ('')
-    const [username, setUsername] = ('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate('/profile');
+    };
+
+
     return (
         <main className="login bg-dark">
             <section className="sign-in-content">
@@ -20,32 +23,37 @@ const Login = () => {
                 <h2>Sign In</h2>
                 <form onSubmit={handleSubmit}>
 
-                <div class="input-wrapper">
-                        <label for="username">Username</label>
+                    <div class="input-wrapper">
+                    <label for="email">Email</label>
                         <input
-                        type="username"
-                        id="username"
-                        value={username}
-                        />
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required />
+                        
                     </div>
 
                     <div class="input-wrapper">
                         <label for="password">Password</label>
                         <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        />
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required/>
                     </div>
 
                     <div className='form-check'>
-            <input
-             type="checkbox"
-             id="rememberMe"
-             />
-             <label>Remember Me</label>
-             </div>
-             <button type="submit" class="sign-in-button">Sign In</button>
+                        <input
+                            type="checkbox"
+                            checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+
+                       
+                        <label>Remember Me</label>
+                    </div>
+                    <button type="submit" class="sign-in-button">Sign In</button>
                 </form>
             </section>
         </main>
